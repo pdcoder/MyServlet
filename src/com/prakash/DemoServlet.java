0575e79eea2cc0684ac4251ed43dbb41d06eee8c
Prakash <prakashdas1998.pd@gmail.com>
2018-09-20 23:44:17 +0530

    Initialized project

---
+++ src/com/prakash/DemoServlet.java
@@ -0,0 +1,21 @@
+package com.prakash;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class DemoServlet
+ */
+@WebServlet("/DemoServlet")
+public class DemoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DemoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+}
